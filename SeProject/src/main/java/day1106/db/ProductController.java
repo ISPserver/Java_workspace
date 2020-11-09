9fafb07b43357fc0aed42934583d1d33b8ba12f8
ISPserver <tjdals080@naver.com>
2020-11-09 19:47:21 +0900

    Socket Echo Server & Shopping App Finish (GUI)

--- SeProject/src/main/java/day1106/db/ProductController.java
+++ SeProject/src/main/java/day1106/db/ProductController.java
@@ -1,37 +1,25 @@
-/*
- * JTable은 디자인에 불과하므로, 이 클래스로부터 데이터에 대한 정보를 가져간다.
- * 따라서 TableModel 인터페이스를 구현한 객체인 Ab
- * */
+/*JTable은 디자인에 불과하므로, 이클래스로부터 데이터에 대한 정보를 가져간다 
+ * 따라서 TableModel 인터페이스를 구현한 객체인 AbstractTableModel을 상속받자 ! */
 package day1106.db;
 
 import javax.swing.table.AbstractTableModel;
 
-public class ProductController extends AbstractTableModel {
-	String[][] data = {};
-	String[]  column = {"product_id","subcategory_id","product_name","brand","price","filename"};
-	
-	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return data.length;
-	}
+public class ProductController extends AbstractTableModel{
+   String [][] data= {};
+   String [] column= {"product_id","subcategory_id","product_name","band","price","filename"};
+   
+   public int getRowCount() {
+      return data.length;
+   }
+   public int getColumnCount() {
+      return column.length;
+   }
+   //JTable이 getColumnCount()의 갯수만큼 호출하면서 순서대로 0,1,2,3,4,5 를 넘기면서 컬럼제목을 가져간다 
+   public String getColumnName(int col) {
+      return column[col];
+   }
+   public Object getValueAt(int row, int col) {
+      return data[row][col];
+   }
 
-	@Override
-	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return column.length;
-	}
-	
-	//JTable이 getColumnCount()의 갯수만큼 호출하며 순서대로0,1,2,3,4,5를 넘기며 컬럼제목 가져간다.
-	@Override
-	public String getColumnName(int col) {
-		return column[col];
-	}
-	
-	@Override
-	public Object getValueAt(int row, int col) {
-		// TODO Auto-generated method stub
-		return data[row][col];
-	}
-	
 }
