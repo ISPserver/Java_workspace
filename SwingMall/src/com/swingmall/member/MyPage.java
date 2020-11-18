5ed496599fac3d0b5a5e7707fcad5956f0d2b10f
ISPserver <tjdals080@naver.com>
2020-11-18 18:51:39 +0900

    Shoping Mall Project by GUI at Swing start

---
+++ SwingMall/src/com/swingmall/member/MyPage.java
@@ -0,0 +1,12 @@
+package com.swingmall.member;
+
+import com.swingmall.main.Page;
+import com.swingmall.main.ShopMain;
+
+public class MyPage extends Page{
+
+	public MyPage(ShopMain shopmain) {
+		super(shopmain);
+	}
+
+}
---
+++ SwingMall/src/com/swingmall/product/Product.java
@@ -0,0 +1,15 @@
+package com.swingmall.product;
+
+import java.awt.Color;
+
+import com.swingmall.main.Page;
+import com.swingmall.main.ShopMain;
+
+public class Product extends Page{
+
+	public Product(ShopMain shopmain) {
+		super(shopmain);
+		this.setBackground(Color.orange);
+	}
+	
+}
